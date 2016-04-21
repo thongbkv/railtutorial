97b6d0701eb6ae1321b6471a10a88af1cc78aa70
thongbkv <framgia@framgia0160-lt.framgia.com>
2016-04-21 11:41:26 +0700

    chapter 5: filling in the layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-
-  get 'static_pages/help'
-
-  get 'static_pages/about'
+  get 'help' => 'static_pages#help'
+  get 'about'=> 'static_pages#about'
+  get 'contact' => 'static_pages#contact'
+  get 'signup' => 'users#new'
 
   resources :microposts
   resources :users
   #root 'application#hello'
-  root 'users#index'
+  root 'static_pages#home'
 end
