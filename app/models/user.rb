885870925674880785a9d42ea4740a38074f965f
thongbkv <framgia@framgia0160-lt.framgia.com>
2016-04-22 08:26:57 +0700

    chapter 6: modeling user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ActiveRecord::Base
 	has_many :microposts
-	validates :name, presence: true
-	validates :email, presence: true
+	before_save {self.email = email.downcase }
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates :name, presence: true, length: { maximum: 50 }
+	validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX}
+
+	has_secure_password
 end
