290606f20b98344b6a7e959ffe89129cee84dd99
thongbkv <framgia@framgia0160-lt.framgia.com>
2016-04-22 14:53:58 +0700

    Finish user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
 	before_save {self.email = email.downcase }
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :name, presence: true, length: { maximum: 50 }
-	validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX}
+	validates :email, presence: true, length: { maximum: 255 } #, format: { with: VALID_EMAIL_REGEX}
 
 	has_secure_password
 end
