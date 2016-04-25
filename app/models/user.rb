88f68e81b7cf562ece6d922fcfe54c1a5db35da1
thongbkv <framgia@framgia0160-lt.framgia.com>
2016-04-25 18:40:58 +0700

    Finish log in/log out

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+	attr_accessor :remember_token
+
 	has_many :microposts
 	before_save {self.email = email.downcase }
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -10,8 +12,28 @@ class User < ActiveRecord::Base
 
 	# Returns the hash digest of the given string.
 	def User.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                  BCrypt::Engine.cost
-    BCrypt::Password.create(string, cost: cost)
-  end
+	    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+	                                                  BCrypt::Engine.cost
+	    BCrypt::Password.create(string, cost: cost)
+  	end
+
+  	# Returns a random token
+  	def User.new_token
+  		SecureRandom.urlsafe_base64
+  	end
+
+  	def remember
+  		self.remember_token = User.new_token
+  		update_attribute(:remember_digest, User.digest(remember_token))
+  	end
+
+  	def authenticated?(remember_token)
+  		return false if remember_digest.nil?
+  		BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  	end
+
+  	# Forgets a user.
+  	def forget
+  		update_attribute(:remember_digest, nil)
+  	end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160421072852) do
+ActiveRecord::Schema.define(version: 20160425055734) do
 
   create_table "microposts", force: :cascade do |t|
     t.text     "content"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20160421072852) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
+    t.string   "remember_digest"
   end
 
 end
